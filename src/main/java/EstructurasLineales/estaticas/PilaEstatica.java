b3f5f50dd3f37d80f0a54a2073155064c2c578f1
Notebook <>
2021-08-10 16:55:58 -0300

    Pequeños pasos

---
+++ src/main/java/EstructurasLineales/estaticas/PilaEstatica.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EstructurasLineales.estaticas;
+
+/**
+ *
+ * @author Sabina
+ */
+public class PilaEstatica {
+    
+    final int TAMANIO;
+    Object[] arreglo;
+    int tope = -1;
+    
+    public PilaEstatica(int Tam){
+        TAMANIO = Tam;
+    }
+    
+    
+    
+    
+}
